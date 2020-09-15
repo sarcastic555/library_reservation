000d44d66cfb1f1f19ba55276a6cfec5160c45f2
Naoki Nagakura <nagakura@nagakuranaokis-MacBook-Pro.local>
2020-09-15 13:05:24 +0900

    delete unnecessary statement

--- create_owning_or_reserving_booklist_ichikawa.py
+++ create_owning_or_reserving_booklist_ichikawa.py
@@ -5,17 +5,6 @@ import datetime
 import logging
 import pandas as pd
 
-# module = tool_ichikawa.IchikawaModule()
-# module.set_debug_mode(True)
-# module.set_sleep_time(3) ##sleep time [sec]
-# print("executing module.get_mypage_book_df('lend')")
-# df_lend=module.get_mypage_book_df('lend') ## 'lend' or 'reserve'
-# print("executing module.get_mypage_book_df('reserve')")
-# df_reserve=module.get_mypage_book_df('reserve') ## 'lend' or 'reserve'
-# df = pd.concat([df_lend,df_reserve])
-# df.to_csv('list/nowreading.csv')
-
-
 class RentalBookInfo:
   def __init__(self):
     self.title = ""
